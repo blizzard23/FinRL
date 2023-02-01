aa3188a8b997076f9ad5d2353d27c52a425e5349
blizzard23 <38344789+blizzard23@users.noreply.github.com>
2023-02-01 21:20:36 +0100

    Update train.py

--- finrl/train.py
+++ finrl/train.py
@@ -6,7 +6,7 @@ from finrl.config import RLlib_PARAMS
 from finrl.config import SAC_PARAMS
 from finrl.config import TRAIN_END_DATE
 from finrl.config import TRAIN_START_DATE
-from finrl.config_tickers import DOW_30_TICKER
+from finrl.config_tickers import MY_TICKER
 from finrl.meta.data_processor import DataProcessor
 from finrl.meta.env_stock_trading.env_stocktrading_np import StockTradingEnv
 
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     train(
         start_date=TRAIN_START_DATE,
         end_date=TRAIN_END_DATE,
-        ticker_list=DOW_30_TICKER,
+        ticker_list=MY_TICKER,
         data_source="yahoofinance",
         time_interval="1D",
         technical_indicator_list=INDICATORS,
