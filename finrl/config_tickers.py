2614733b2a96133c51b4ae8747454162c15026bb
blizzard23 <38344789+blizzard23@users.noreply.github.com>
2023-02-01 17:54:54 +0100

    Update config_tickers.py

--- finrl/config_tickers.py
+++ finrl/config_tickers.py
@@ -2,6 +2,21 @@ from __future__ import annotations
 
 SINGLE_TICKER = ["AAPL"]
 
+MY_TICKER = [
+    "NEE",
+    "TSLA",
+    "UL",
+    "DNNGY",
+    "FSLR",
+    "HAIN",
+    "DPSGY",
+    "SAP",
+    "NVO",
+    "SYK",
+    "CLPBY",
+    "SEDG",
+]
+
 # Dow 30 constituents in 2021/10
 # check https://wrds-www.wharton.upenn.edu/ for U.S. index constituents
 DOW_30_TICKER = [
