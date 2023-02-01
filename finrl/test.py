a9e82e8e576fe622f14990ef73637954840d7562
blizzard23 <38344789+blizzard23@users.noreply.github.com>
2023-02-01 21:18:56 +0100

    Update test.py

--- finrl/test.py
+++ finrl/test.py
@@ -4,7 +4,7 @@ from finrl.config import INDICATORS
 from finrl.config import RLlib_PARAMS
 from finrl.config import TEST_END_DATE
 from finrl.config import TEST_START_DATE
-from finrl.config_tickers import DOW_30_TICKER
+from finrl.config_tickers import MY_TICKER
 from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
 
 
@@ -92,7 +92,7 @@ if __name__ == "__main__":
     account_value_erl = test(
         start_date=TEST_START_DATE,
         end_date=TEST_END_DATE,
-        ticker_list=DOW_30_TICKER,
+        ticker_list=MY_TICKER,
         data_source="yahoofinance",
         time_interval="1D",
         technical_indicator_list=INDICATORS,
